e5abf178231df882f5768da3b138645a894fe7d5
xioperez01 <angie.xioperez@gmail.com>
2020-09-01 18:07:37 +0000

    Errasing format

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -8,10 +8,10 @@ else:
         number2 = number % 10
 
 if number2 > 5:
-            print("Last digit of {:d} is {:d} and is greater than 5"
+            print("Last digit of {} is {} and is greater than 5"
                             .format(number, number2))
 if number2 is 0:
-    print("Last digit of {:d} is {:d} and is 0".format(number, number2))
+    print("Last digit of {} is {} and is 0".format(number, number2))
 else:
-    print("Last digit of {:d} is {:d} and is less than 6 and not 0"
+    print("Last digit of {} is {} and is less than 6 and not 0"
           .format(number, number2))
