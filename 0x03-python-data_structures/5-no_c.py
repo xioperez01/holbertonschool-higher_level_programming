22154c4ed37e706b504f5f9ee0849e677aa0af51
Angie Pérez <66028978+xioperez01@users.noreply.github.com>
2020-09-08 20:58:46 -0500

    Adding second for

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,9 +2,11 @@
 def no_c(my_string):
     if my_string:
         aux = list(my_string)
-        for i in aux:
-            if i == 'c' or i == 'C':
-                aux.remove(i)
+        for i in range(aux.count('c')):
+            aux.remove('c')
+        
+        for i in range(aux.count('C')):
+            aux.remove('C')
 
         new = "".join(aux)
         return new
