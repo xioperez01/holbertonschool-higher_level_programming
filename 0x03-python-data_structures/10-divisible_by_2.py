5c54f51a58da5604b8f4473cab45004b6ed39d32
xioperez01 <angie.xioperez@gmail.com>
2020-09-09 03:13:29 +0000

    Initial code task 10

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    new = []
+    i = 0
+    while i < len(my_list):
+        if my_list[i] % 2 == 0:
+            new.append(True)
+        else:
+            new_.append(False)
+            i+= 1
+
+    return new
