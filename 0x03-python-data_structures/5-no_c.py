2b9273fed517bcf6668452c9b67da50665df90e5
xioperez01 <angie.xioperez@gmail.com>
2020-09-09 01:46:25 +0000

    Errasing indices

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,8 +3,8 @@ def no_c(my_string):
     if my_string:
         aux = list(my_string)
         for i in range(len(my_string)):
-            if my_string[i] == 'c' or my_string[i] == 'C':
-                aux.remove(aux[i])
+            if i == 'c' or i == 'C':
+                aux.remove(i)
 
         aux = "".join(aux)
         return aux
