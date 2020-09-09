1e8b509f132650d78de91b9b2a0bbf3ad2ec385d
Angie Pérez <66028978+xioperez01@users.noreply.github.com>
2020-09-08 20:48:08 -0500

    Errasing range

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,7 +2,7 @@
 def no_c(my_string):
     if my_string:
         aux = list(my_string)
-        for i in range(len(my_string)):
+        for i in aux:
             if i == 'c' or i == 'C':
                 aux.remove(i)
 
