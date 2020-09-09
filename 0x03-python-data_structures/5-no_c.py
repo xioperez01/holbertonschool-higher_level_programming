55518b6ca8326ccaff81437411b64260f1bc0c2a
Angie Pérez <66028978+xioperez01@users.noreply.github.com>
2020-09-08 21:14:46 -0500

    Errasing if

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    if my_string:
-        aux = list(my_string)
-        for i in aux:
-            if i == 'c' or i == 'C':
-                aux.remove(i)
-
-        new = "".join(aux)
-        return new
+    aux = list(my_string)
+    for i in aux:
+        if i == 'c' or i == 'C':
+            aux.remove(i)
+    new = "".join(aux)
+    return new
