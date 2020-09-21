e04a54a33c370e30e92b020e8828628931aaa326
xioperez01 <angie.xioperez@gmail.com>
2020-09-21 22:08:23 +0000

    Print void and return in right place

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -7,5 +7,5 @@ def safe_print_list_integers(my_list=[], x=0):
             j += 1
         except (TypeError, ValueError):
             continue
-        print()
-        return j
+    print()
+    return j
