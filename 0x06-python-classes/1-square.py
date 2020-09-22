7c7540bf1a33dc81fc603abdb4e39082032b470d
xioperez01 <angie.xioperez@gmail.com>
2020-09-22 21:19:27 +0000

    Initial code task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Module 1-square
+Define class Square
+"""
+
+
+class Square:
+    """
+    Define a square
+
+    Args:
+        size : Side of a square
+    """
+    def __init__(self, size):
+        """
+        Initializes square
+
+        Atributes:
+                 size : Side of a square
+        """
+        self.__size = size
