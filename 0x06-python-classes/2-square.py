786a80bd0714eee89388ddbfa5d9fa684a9c3889
xioperez01 <angie.xioperez@gmail.com>
2020-09-22 21:38:25 +0000

    Initial commit task 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Module 2-square
+Define class Square
+"""
+
+
+class Square:
+    """Class for size square """
+    def __init__(self, size=0):
+
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
