d9f8c6f141c657c8b7d3e27d63298dafc4f0f993
xioperez01 <angie.xioperez@gmail.com>
2020-09-23 03:23:34 +0000

    set new value to position

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -38,6 +38,7 @@ class Square:
            type(value[0]) != int or type(value[1]) != int or \
            value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
 
     def my_print(self):
         if self.__size == 0:
