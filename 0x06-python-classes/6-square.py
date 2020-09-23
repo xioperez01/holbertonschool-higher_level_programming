8eed953c74c0085a39a67da51e8955936dac80a7
Angie Pérez <66028978+xioperez01@users.noreply.github.com>
2020-09-22 22:26:01 -0500

    Update 6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -44,7 +44,7 @@ class Square:
         if self.__size == 0:
             print()
         else:
-            for x in range(self.position[1]):
+            for x in range(self.__position[1]):
                 print()
             for i in range(self.__size):
                 for k in range(self.__position[0]):
