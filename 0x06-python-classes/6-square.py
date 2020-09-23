7342a4733e9ac497639f6acbd2218acb8f4ff1a1
Angie Pérez <66028978+xioperez01@users.noreply.github.com>
2020-09-22 22:28:35 -0500

    Correct size

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -49,6 +49,6 @@ class Square:
             for i in range(self.__size):
                 for k in range(self.__position[0]):
                     print(" ", end="")
-                for j in range(self.__size + self.__position[0]):
+                for j in range(self.__size):
                     print("#", end="")
                 print()
