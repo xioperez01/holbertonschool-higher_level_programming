4a1435a1f722566432a7b0274bd7b05a234f2ae2
Angie Pérez <66028978+xioperez01@users.noreply.github.com>
2020-09-28 19:22:07 -0500

    Update 1-rectangle.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -7,8 +7,8 @@ Define class Rectangle
 class Rectangle:
     """Initializes square"""
     def __init__(self, width=0, height=0):
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
