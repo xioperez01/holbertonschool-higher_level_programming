1b65c061fa89cecaba8938ea6c081e7d7683a7c1
xioperez01 <angie.xioperez@gmail.com>
2020-09-29 02:09:14 +0000

    Pycodestyle

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -7,6 +7,7 @@ Define class Rectangle
 class Rectangle:
     """Initializes square"""
     number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
