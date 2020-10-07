0321de5687e46fc083d3c5b29db994da05d86c77
xioperez01 <angie.xioperez@gmail.com>
2020-10-07 00:12:35 +0000

    Initial code task 0

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Function that returns the list of available attributes
+and methods of an object:
+"""
+
+
+def lookup(obj):
+        """Returns a list object"""
+        return dir(obj)
