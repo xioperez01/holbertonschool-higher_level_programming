d0d507ac8e824c6580efb99f05dae33c658fe2ac
xioperez01 <angie.xioperez@gmail.com>
2020-10-08 01:29:59 +0000

    Pycodestyle

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+Module 0-read_file.py
+"""
+
+
 def read_file(filename=""):
     """ reads a text file (UTF8) and prints it to stdout"""
     with open(filename, 'r', encoding="utf-8") as f:
