ca3dadb83482fc3784a7b0b148d7d6d6ea548a5d
xioperez01 <angie.xioperez@gmail.com>
2020-10-08 00:26:16 +0000

    Initial code task 10

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Module 10-square.py
+"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ Square class"""
+    def __init__(self, size):
+        """ Constructor """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
