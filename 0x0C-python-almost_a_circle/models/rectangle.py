c774fa25752dbb2393e647aeb7e135463552cda7
xioperez01 <angie.xioperez@gmail.com>
2020-10-13 03:46:09 +0000

    Rectangle file

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,75 +1,43 @@
-'''
+#!/usr/bin/python3
+"""
 Created on 12/10/2020
 
 @author: xioperez
-'''
-
-from models.base import Base
+"""
 
 
-class Rectangle(Base):
-    '''
-    class Rectangle
-    '''
+import json
 
-    def __init__(self, width, height, x=0, y=0, id=None):
-        '''
-        Constructor
-        '''
-        super().__init__(id)
-        self.width = width
-        self.height = height
-        self.x = x
-        self.y = y
 
-    @property
-    def width(self):
-        return self.__width
+class Base(object):
+    """ class Base """
+    __nb_objects = 0
 
-    @property
-    def height(self):
-        return self.__height
-
-    @property
-    def x(self):
-        return self.__x
-
-    @property
-    def y(self):
-        return self.__y
-
-    @width.setter
-    def width(self, value):
-        if type(value) != int:
-            raise TypeError("width must be an integer")
-        elif value <= 0:
-            raise ValueError("width must be > 0")
+    def __init__(self, id=None):
+        """ Constructor """
+        if id is not None:
+            self.id = id
         else:
-            self.__width = value
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
 
-    @height.setter
-    def height(self, value):
-        if type(value) != int:
-            raise TypeError("height must be an integer")
-        elif value <= 0:
-            raise ValueError("height must be > 0")
-        else:
-            self.__height = value
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """ returns the JSON string representation
+        of list_dictionaries"""
+        if list_dictionaries is None:
+            return "[]"
+        return json.dumps(list_dictionaries)
 
-    @x.setter
-    def x(self, value):
-        if type(value) != int:
-            raise TypeError("x must be an integer")
-        elif value < 0:
-            raise ValueError("x must be >= 0")
-        else:
-            self.__x = value
+    """def save_to_file(cls, list_objs):"""
 
-    @y.setter
-    def y(self, value):
-        if type(value) != int:
-            raise TypeError("y must be an integer")
-        elif value < 0:
-            raise ValueError("y must be >= 0")
+    @staticmethod
+    def from_json_string(json_string):
+        """
+        Returns the list of the JSON string
+        representation json_string:
+        """
+        if json_string is None or len(json_string) == 0:
+            return []
         else:
-            self.__y = value
+            return json.loads(json_string)
