66b5a4c8277a908d0b7ff7ccd27f3365c6de27ff
xioperez01 <angie.xioperez@gmail.com>
2020-12-18 20:33:52 +0000

    Final code

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -17,8 +17,8 @@ if __name__ == "__main__":
                          db=argv[3])
 
     cursor = db.cursor
-    up = "SELECT * FROM states WHERE name = '{}' ORDER BY states.id ASC"\
-        .format(argv[4])
+    up = "SELECT * FROM states WHERE name = '{:s}' ORDER BY states.\
+id ASC".format(argv[4])
     cursor.execute(up)
     list_ = cursor.fetchall()
     for row in list_:
