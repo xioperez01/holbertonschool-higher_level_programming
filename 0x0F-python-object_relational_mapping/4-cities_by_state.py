455557ef45ebc7f5bcdfcb162d07619476c2b89b
xioperez01 <angie.xioperez@gmail.com>
2020-12-18 22:07:30 +0000

    changing

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """
-Script that lists all cities from the database hbtn_0e_4_usa
-take 3 arguments: mysql username, mysql password and database nam
+return info from both tables (tables 'cities' 'states)
+parameters given to script: username, password, database
 """
 
 import MySQLdb
 from sys import argv
 
-if __name__ = "__main__":
+if __name__ == "__main__":
 
     db = MySQLdb.connect(host="localhost",
                          port=3306,
@@ -15,12 +15,15 @@ if __name__ = "__main__":
                          passwd=argv[2],
                          db=argv[3])
 
+    """Connect to a MySQL server."""
+
     cursor = db.cursor()
     cmd = """SELECT cities.id, cities.name, states.name
                  FROM states
                  INNER JOIN cities ON states.id = cities.state_id
                  ORDER BY cities.id ASC"""
     cursor.execute(cmd)
+
     for row in cursor.fetchall():
         print(row)
     cursor.close()
