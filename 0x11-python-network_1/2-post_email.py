86f9fbf4169575f79621900dcdc44c385fb96f9c
xioperez01 <angie.xioperez@gmail.com>
2021-01-13 18:02:46 +0000

    Initial code

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Module that takes in a URL and an email,
+sends a POST request to the passed URL with the email
+as a parameter, and displays the body of the response 
+"""
+
+import urllib.request
+import urllib.parse
+from sys import argv
+
+if __name__ == "__main__":
+    url = argv[1]
+    values = {'mail': argv[2]}
+    data = urllib.parse.urlencode(values)
+    data = data.encode('ascii')
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as response:
+        the_page = response.read()
+        print("{}".format(the_page.decode('utf-8')))
