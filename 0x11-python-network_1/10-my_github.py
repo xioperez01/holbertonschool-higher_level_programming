620268a26d95f5dd44aded4401f0a2d554a802d1
xioperez01 <angie.xioperez@gmail.com>
2021-01-13 21:01:11 +0000

    Initial code

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,13 @@
+"""
+Module that takes a Github credentials (username and password)
+and uses the Github API to display the id
+"""
+from sys import argv
+import requests
+from requests.auth import HTTPBasicAuth
+
+
+if __name__ == "__main__":
+    url = 'https://api.github.com/user'
+    r = requests.get(url, auth=HTTPBasicAuth(argv[1], argv[2]))
+    print(r.json().get('id'))
