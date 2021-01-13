f18febdbfa20ffaba126d63042efb38daf91602c
xioperez01 <angie.xioperez@gmail.com>
2021-01-13 18:07:24 +0000

    Initial code

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -11,10 +11,10 @@ from sys import argv
 
 if __name__ == "__main__":
     url = argv[1]
-    values = {'mail': argv[2]}
+    values = {'email': argv[2]}
     data = urllib.parse.urlencode(values)
     data = data.encode('ascii')
     req = urllib.request.Request(url, data)
     with urllib.request.urlopen(req) as response:
         the_page = response.read()
-        print("{}".format(the_page.decode('utf-8')))
+        print(response.read().decode('utf-8'))
