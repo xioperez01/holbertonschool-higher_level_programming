bcacd4f849aaf0755a61ba4f8b6ad75cfc03e0c7
xioperez01 <angie.xioperez@gmail.com>
2021-01-13 18:17:03 +0000

    Pycodestyle

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -2,7 +2,7 @@
 """
 Module that takes in a URL and an email,
 sends a POST request to the passed URL with the email
-as a parameter, and displays the body of the response 
+as a parameter, and displays the body of the response
 """
 
 import urllib.request
