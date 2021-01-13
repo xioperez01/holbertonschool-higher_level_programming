8fb4ff68a876be154de6e25b86162fac443bf8c4
xioperez01 <angie.xioperez@gmail.com>
2021-01-13 19:43:23 +0000

    Initial code

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Module that takes in a URL, sends a request to the URL
+and displays the value of the variable X-Request-Id in
+the response header with requests and sys packages
+"""
+
+from sys import argv
+import requests
+
+if if __name__ == "__main__":
+    r = requests.get(argv[1])
+    print(r.headers.get('X-Request-Id'))
