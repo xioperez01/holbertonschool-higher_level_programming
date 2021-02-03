f30bed3866c6ae1250585b791c240aa6bc144762
xioperez01 <angie.xioperez@gmail.com>
2021-02-03 01:25:29 +0000

    New File: 11-second_biggest.js

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const arrayNumbers = process.argv.slice(2);
+function secondMax (array) {
+  if (array.length < 2) {
+    return (0);
+  } else {
+    array.sort((x, y) => x - y);
+    array.pop();
+    return (array.pop());
+  }
+}
+console.log(secondMax(arrayNumbers));
