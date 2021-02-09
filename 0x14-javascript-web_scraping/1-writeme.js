7cb1bcfdb79a27dddba4eea3f8943adb7fa4c651
xioperez01 <angie.xioperez@gmail.com>
2021-02-09 14:20:16 -0500

    taask 0 file: script that writes a string to a file.

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const fileName = process.argv[2];
+const toWrite = process.argv[3];
+const fs = require('fs');
+fs.appendFile(fileName, toWrite, 'utf-8', function (error) {
+  if (error) {
+    console.log(error);
+  }
+});
