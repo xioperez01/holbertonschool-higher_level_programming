b93cfdc88dc5a5d1455ca641c5832388f84515e5
xioperez01 <angie.xioperez@gmail.com>
2021-02-09 02:30:46 +0000

    Correct ouput

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -5,6 +5,6 @@ logMe Function
 let count = 0;
 exports.logMe = function (item) {
   // prints <number arguments already printed>: <current argument value>
-  console.log(count + ':' + item);
+  console.log(count + ': ' + item);
   count += 1;
 };
