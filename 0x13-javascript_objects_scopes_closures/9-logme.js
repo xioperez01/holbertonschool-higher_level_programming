2748a25251f94b8dfb88aca1ff28eed3395f70ba
xioperez01 <angie.xioperez@gmail.com>
2021-02-09 02:29:15 +0000

    Task 9 files

---
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+/*
+logMe Function
+*/
+let count = 0;
+exports.logMe = function (item) {
+  // prints <number arguments already printed>: <current argument value>
+  console.log(count + ':' + item);
+  count += 1;
+};
