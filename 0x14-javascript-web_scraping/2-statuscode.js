9b1b63bc938bde3abf5de566a69a1be813389e26
xioperez01 <angie.xioperez@gmail.com>
2021-02-09 14:47:37 -0500

    Task 2 file: script that display the status code of a GET request.

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const url = process.argv[2];
+const request = require('request');
+request(url, function (error, response) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log('code: ' + response.statusCode);
+  }
+});
