0cc4610ea89382c471cba2af2537627d4fa091af
xioperez01 <angie.xioperez@gmail.com>
2021-02-09 14:52:20 -0500

    correct ouput x 2

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -5,6 +5,6 @@ request(url, function (error, response) {
   if (error) {
     console.error(error);
   } else {
-    console.log('code: ', response && response.statusCode);
+    console.log('code:', response && response.statusCode);
   }
 });
